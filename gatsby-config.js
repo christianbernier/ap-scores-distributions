47af4cdf7558f094c9b4228b5b0b7973815f91d3
christianbernier <64christianb@gmail.com>
2020-07-07 01:38:17 -0400

    Fixed config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,6 @@
 module.exports = {
   plugins: [
     "gatsby-plugin-emotion",
-    "@rhysforyou/gatsby-plugin-react-helmet-async",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "data",
-        path: `${__dirname}/src/data`
-      }
-    },
-    "gatsby-transformer-csv"
+    "@rhysforyou/gatsby-plugin-react-helmet-async"
   ]
 }
